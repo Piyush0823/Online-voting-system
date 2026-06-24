c50ba1c43b4b1ea924a2ef0d33be65065b0b7c82
Piyush Verma <162396912+Piyush0823@users.noreply.github.com>
2026-06-24 16:18:07 +0530

    Implement choice-based script execution in run.bat
    Added handling for user choices to run different Python scripts.

--- run.bat
+++ run.bat
@@ -30,3 +30,28 @@ set /p choice=Enter choice [1-4]:
 
 if "%choice%"=="1" goto run_web
 
+if "%choice%"=="2" goto run_gui
+if "%choice%"=="3" goto run_cli
+if "%choice%"=="4" goto end
+
+echo Invalid choice. Exiting.
+pause
+exit /b 1
+
+:run_web
+echo Starting web_app.py...\r\n
+"%PYTHON_EXEC%" web_app.py
+goto end
+
+:run_gui
+echo Starting gui_voting_system.py...\r\n
+"%PYTHON_EXEC%" gui_voting_system.py
+goto end
+
+:run_cli
+echo Starting final.py...\r\n
+"%PYTHON_EXEC%" final.py
+goto end
+
+:end
+endlocal
